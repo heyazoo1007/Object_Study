7e36f192a2b0d6171d8188943eb858a0512391b5
heyazoo1007 <heyazoo1007@gmail.com>
2022-05-08 22:11:23 +0900

    객체지향 공부 시작

---
+++ src/main/java/Chapter_1/Invitation.java
@@ -0,0 +1,8 @@
+package Chapter_1;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+
+    private LocalDateTime when;
+}
---
+++ src/main/java/Chapter_1/Ticket.java
@@ -0,0 +1,10 @@
+package Chapter_1;
+
+public class Ticket {
+
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
