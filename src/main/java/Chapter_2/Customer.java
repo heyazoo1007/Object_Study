2a79a2777dc59e28209d171c3ba7e8d99c17a170
heyazoo1007 <heyazoo1007@gmail.com>
2022-05-20 19:31:41 +0900

    나머지

--- src/main/java/Chapter_2/Customer.java
+++ src/main/java/Chapter_2/Customer.java
@@ -1,4 +1,12 @@
 package Chapter_2;
 
 public class Customer {
+
+    private String name;
+    private String id;
+
+    public Customer(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
 }
